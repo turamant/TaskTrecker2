52524db6acd451d30f64c6700a3931ad03e68ea4
askvart <tur1amant@gmail.com>
2021-10-27 11:28:13 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+Трекер заданий
+
+<h2>Install</h2>
+<p>pip install Flask</p>
+<p>pip install pytest</p>
+<p>pip install coverage</p>
+
+<h2>Initialize the Database File</h2>
+<p>flask init-db</p>
+
+<h2>Task Trecker</h2>
+<p>backend: Flask</p>
+<p>app: task</p>
+<p>db: sqlite</p>
--- tasktrecker/task.py
+++ tasktrecker/task.py
@@ -13,7 +13,7 @@ def index():
     db = get_db()
     tasks = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM task p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('task/index.html', tasks=tasks)
@@ -35,7 +35,7 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO task (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
@@ -47,7 +47,7 @@ def create():
 def get_task(id, check_author=True):
     task = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM task p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
@@ -78,7 +78,7 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE task SET title = ?, body = ?'
                 ' WHERE id = ?',
                 (title, body, id)
             )
@@ -93,6 +93,6 @@ def update(id):
 def delete(id):
     get_task(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM task WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('task.index'))
